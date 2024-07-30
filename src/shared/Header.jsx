dced72f182af8028eff4148a8ebbd7bc54ae5377
TANVIR0222 <tanvirislam3912@gmail.com>
2024-07-30 22:56:00 +0600

    update

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,4 +1,4 @@
-import { Button, Navbar, TextInput } from "flowbite-react";
+import { Button, Dropdown, Avatar, Navbar, TextInput } from "flowbite-react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
 import { Link, useLocation } from "react-router-dom";
@@ -8,6 +8,34 @@ const Header = () => {
   return (
     <div>
       <Navbar className="border-b-2">
+{/* update */}
+      <Navbar.Brand href="https://flowbite-react.com">
+        <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+      </Navbar.Brand>
+      <div className="flex md:order-2">
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
+          }
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">Bonnie Green</span>
+            <span className="block truncate text-sm font-medium">name@flowbite.com</span>
+          </Dropdown.Header>
+          <Dropdown.Item>Dashboard</Dropdown.Item>
+          <Dropdown.Item>Settings</Dropdown.Item>
+          <Dropdown.Item>Earnings</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item>Sign out</Dropdown.Item>
+        </Dropdown>
+        <Navbar.Toggle />
+        </div>
+
+{/* update */}
+tnn.sdfn.gdf
         <Link
           to={"/"}
           className=" self-center whitespace-nowrap  text-sm sm:text-xl font-semibold dark:text-white"
